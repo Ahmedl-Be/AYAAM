e2f831e9a885c08753cdcc9ec7bae7807e6a1dcf
Azza Sallam <Azzasallam369@gmail.com>
2025-08-16 03:13:52 +0300

    Addes My Data Which Contain Women Category for Dresses And Accessories

---
+++ data/DataCollection-Azza/data.js
@@ -0,0 +1,597 @@
+export const azzaProducts = [
+  {
+    id: 'prod_1' ,
+    name: "Short Dress", 
+    description: "White Short Sleeve Formal Occasion Elegant Slim A-Line Dress",
+    price: 5.68,
+    sellerId: 'seller_2' ,
+    category: "Women", 
+    subCategory: "Dresses" ,
+    
+    stock: [
+      { size: "S", color: "White", quantity: 5 },
+      { size: "M", color: "White", quantity: 7 },
+      { size: "L", color: "White", quantity: 3 },
+
+      { size: "S", color: "Black", quantity: 4 },
+      { size: "M", color: "Black", quantity: 6 },
+      { size: "L", color: "Black", quantity: 2 } ,
+
+      { size: "S", color: "BabyBlue", quantity: 2 } ,
+      { size: "M", color: "BabyBlue", quantity: 2 } ,
+      { size: "L", color: "BabyBlue", quantity: 2 } ,
+    ],
+
+
+    images: {
+      White: "assets/dresses/dress1-white.png",
+      Black: "assets/dresses/dress1-black.png" ,
+      BabyBlue : "assets/dresses/dress1-babyblue.png"
+    },
+
+    material: "Cotton",
+    brand: "Zara"
+},
+
+{
+    id: 'prod_2',
+    name: "Casual Dress",
+    description: "Franclia Women's Casual Striped Button Front Dress, Summer Summer Dress Roupa Para A Universidade",
+    price: 750,
+    sellerId: 'seller_1',
+    category: "Women", 
+    subCategory: "Dresses",
+    
+    
+    stock: [
+      { size: "S", color: "White", quantity: 4 },
+      { size: "M", color: "White", quantity: 5 },
+      { size: "L", color: "White", quantity: 3 },
+
+      { size: "M", color: "Black", quantity: 2 } ,
+      { size: "L", color: "Black", quantity: 4 } ,
+      { size: "Xl", color: "Black", quantity: 2 } ,
+
+      { size: "S", color: "BabyBlue", quantity: 3 } ,
+      { size: "L", color: "BabyBlue", quantity: 5 } ,
+
+      { size: "L", color: "Pink", quantity: 1 } ,
+      { size: "Xl", color: "Pink", quantity: 2 } ,
+
+    ],
+
+    images: {
+      Pink: "assets/dresses/dress2-pink.png",
+      White: "assets/dresses/dress2-white.png",
+      Black: "assets/dresses/dress2-black.png",
+      BabyBlue: "assets/dresses/dress2-babyblue.png",
+    },
+
+    material: "Chiffon",
+    brand: "H&M",
+  } ,
+
+  {
+    id: 'prod_3',
+    name: "Classic Short Dress",
+    description: "Solid Color Lapel Neck Short Sleeve Shirt Midi Dress",
+    price: 650,
+    sellerId: 'seller_1',
+    category: "Women", 
+    subCategory: "Dresses",
+    
+    
+    stock: [
+      { size: "S", color: "White", quantity: 4 },
+      { size: "M", color: "White", quantity: 6 },
+      { size: "L", color: "White", quantity: 10 },
+      { size: "Xl", color: "White", quantity: 12 },
+
+      { size: "M", color: "Black", quantity: 2 } ,
+      { size: "L", color: "Black", quantity: 4 } ,
+      { size: "Xl", color: "Black", quantity: 2 } ,
+
+    ],
+
+    images: {
+      White: "assets/dresses/dress3-white.png",
+      Black: "assets/dresses/dress3-black.png",
+    },
+
+    material: "Chiffon",
+    brand: "SheIn"
+  } ,
+
+  {
+    id: 'prod_4',
+    name: "Elegant Long Dress",
+    description: "Black Textured Fabric Button Decor Back Slit Long Sleeve Elegant Long Dress For Women",
+    price: 900,
+    sellerId: 'seller_2',
+    category: "Women", 
+    subCategory: "Dresses",
+    
+    
+    stock: [
+      { size: "Xs", color: "Black", quantity: 2 } ,
+      { size: "S", color: "Black", quantity: 4 } ,
+      { size: "M", color: "Black", quantity: 6 } ,
+      { size: "L", color: "Black", quantity: 10 } ,
+      { size: "Xl", color: "Black", quantity: 12 } ,
+
+    ],
+
+    images: {
+      Black: "assets/dresses/dress4-black.png",
+    },
+
+    material: "Chiffon",
+    brand: "SheIn",
+  } ,
+    {
+    id: 'prod_5',
+    name: "Formal Long Dress",
+    description: "Square Neck 3/4 Sleeve Retro Casual Business Professional OL Bodycon Dress",
+    price: 1200,
+    sellerId: 'seller_2',
+    category: "Women", 
+    subCategory: "Dresses", 
+    
+    
+    stock: [
+      { size: "S", color: "Black", quantity: 4 } ,
+      { size: "M", color: "Black", quantity: 6 } ,
+      { size: "L", color: "Black", quantity: 10 } ,
+      { size: "Xl", color: "Black", quantity: 12 } , 
+
+      { size: "S", color: "Blue", quantity: 4 } , 
+      { size: "M", color: "Blue", quantity: 6} , 
+      { size: "L", color: "Blue", quantity: 8 } , 
+      { size: "Xl", color: "Blue", quantity: 12 } , 
+
+    ],
+
+    images: {
+      Black: "assets/dresses/dress5-black.png",
+      Blue: "assets/dresses/dress5-blue.png",
+    },
+
+    material: "Chiffon", 
+    brand: "SheIn", 
+  } ,
+  {
+    id: 'prod_6',
+    name: "Soiree Long Dress",
+    description: " Round Neck Long Sleeve Crystal Decor Simple Casual Long Dress Long Evening Dresses",
+    price: 950,
+    sellerId: 'seller_3', 
+    category: "Women", 
+    subCategory: "Dresses", 
+    
+    stock: [
+      { size: "S", color: "Black", quantity: 4 } ,
+      { size: "M", color: "Black", quantity: 6 } ,
+      { size: "L", color: "Black", quantity: 8 } ,
+      { size: "Xl", color: "Black", quantity: 12 } , 
+
+      { size: "S", color: "BabyBlue", quantity: 4 } , 
+      { size: "M", color: "BabyBlue", quantity: 6} , 
+      { size: "L", color: "BabyBlue", quantity: 10 } , 
+      { size: "Xl", color: "BabyBlue", quantity: 12 } , 
+
+      { size: "S", color: "OliveGreen", quantity: 4 } , 
+      { size: "M", color: "OliveGreen", quantity: 6 } , 
+      { size: "L", color: "OliveGreen", quantity: 10 } , 
+      { size: "Xl", color: "OliveGreen", quantity: 12 } , 
+
+    ],
+
+    images: {
+      Black: "assets/dresses/dress6-black.png",
+      Blue: "assets/dresses/dress6-babyblue.png",
+      oliveGreen: "assets/dresses/dress6-oliveGreen.png",
+    },
+
+    material: "Chiffon", 
+    brand: "SheIn", 
+  } ,
+
+  {
+    id: 'prod_6',
+    name: "Soiree Long Dress",
+    description: " Round Neck Long Sleeve Crystal Decor Simple Casual Long Dress Long Evening Dresses",
+    price: 950,
+    sellerId: 'seller_3', 
+    category: "Women", 
+    subCategory: "Dresses", 
+    
+    stock: [
+      { size: "S", color: "Black", quantity: 4 } ,
+      { size: "M", color: "Black", quantity: 6 } ,
+      { size: "L", color: "Black", quantity: 8 } ,
+      { size: "Xl", color: "Black", quantity: 12 } , 
+
+      { size: "S", color: "BabyBlue", quantity: 4 } , 
+      { size: "M", color: "BabyBlue", quantity: 6} , 
+      { size: "L", color: "BabyBlue", quantity: 10 } , 
+      { size: "Xl", color: "BabyBlue", quantity: 12 } , 
+
+      { size: "S", color: "OliveGreen", quantity: 4 } , 
+      { size: "M", color: "OliveGreen", quantity: 6 } , 
+      { size: "L", color: "OliveGreen", quantity: 10 } , 
+      { size: "Xl", color: "OliveGreen", quantity: 12 } , 
+
+    ],
+
+    images: {
+      Black: "assets/dresses/dress6-black.png",
+      Blue: "assets/dresses/dress6-babyblue.png",
+      oliveGreen: "assets/dresses/dress6-oliveGreen.png",
+    },
+
+    material: "Chiffon", 
+    brand: "SheIn", 
+  } ,
+
+  {
+    id: 'prod_7',
+    name: " Long Evening Dress",
+    description: "Round Neck Long Sleeve Slit Hem Casual Slim Long Dress Long Evening Dresse",
+    price: 800,
+    sellerId: 'seller_3',
+    category: "Women", 
+    subCategory: "Dresses", 
+    
+    stock: [
+      { size: "S", color: "Black", quantity: 4 } ,
+      { size: "M", color: "Black", quantity: 6 } ,
+      { size: "L", color: "Black", quantity: 8 } ,
+      { size: "Xl", color: "Black", quantity: 12 } , 
+
+      { size: "S", color: "red", quantity: 4 } , 
+      { size: "Xl", color: "red", quantity: 12 } , 
+
+      { size: "L", color: "DarkGreen", quantity: 6 } , 
+      { size: "Xl", color: "DarkGreen", quantity: 2 } , 
+
+      { size: "M", color: "Pink", quantity: 9} , 
+      { size: "Xl", color: "Pink", quantity: 6} , 
+
+    ],
+
+    images: {
+      Black: "assets/dresses/dress7-black.png",
+      Pink: "assets/dresses/dress7-pink.png",
+      DarkGreen: "assets/dresses/dress7-darkGreen.png",
+      Red: "assets/dresses/dress7-red.png",
+    },
+
+    material: "Chiffon", 
+    brand: "SheIn", 
+  } ,
+  {
+    id: 'prod_8',
+    name: " Long Summer Dress",
+    description: "Long Bell Sleeve Asymmetrical Ruffle Hem Dress",
+    price: 1000,
+    sellerId: 'seller_3', 
+    category: "Women", 
+    subCategory: "Dresses", 
+    
+    stock: [
+      { size: "S", color: "Red", quantity: 4 } , 
+      { size: "L", color: "Red", quantity: 6} , 
+
+      { size: "L", color: "Blue", quantity: 6 } , 
+      { size: "Xl", color: "Blue", quantity: 8 } , 
+
+      { size: "M", color: "BabyBlue", quantity: 10} , 
+      { size: "Xl", color: "BabyBlue", quantity: 6} , 
+
+    ],
+
+    images: {
+      BabyBlue: "assets/dresses/dress8-babyblue.png",
+      Blue: "assets/dresses/dress8-blue.png",
+      Red: "assets/dresses/dress8-red.png",
+    },
+
+    material: "Chiffon", 
+    brand: "SheIn",
+  } ,
+  {
+    id: 'prod_9',
+    name: "Flower Long Summer Dress",
+    description: "Women 3D Flower Decor Patchwork Ruffle Hem Dress",
+    price: 1500,
+    sellerId: 'seller_1', 
+    category: "Women", 
+    subCategory: "Dresses", 
+    
+    stock: [
+      { size: "S", color: "Black", quantity: 4 } , 
+      { size: "L", color: "Black", quantity: 6} , 
+      { size: "Xl", color: "Black", quantity: 6} , 
+      { size: "XXl", color: "Black", quantity: 12} , 
+
+    ],
+
+    images: {
+      Black: "assets/dresses/dress9-black.png",
+    },
+
+    material: "Chiffon",
+    brand: "SheIn",
+  } ,
+
+  {
+    id: 'prod_10',
+    name: "Elegant Long Dress",
+    description: "Modelyn Asymmetrical Neck Pleated Detail Dress",
+    price: 500,
+    sellerId: 'seller_2',
+    category: "Women", 
+    subCategory: "Dresses",
+    
+    stock: [
+      { size: "S", color: "Black", quantity: 4 } , 
+      { size: "L", color: "Black", quantity: 6} , 
+      { size: "Xl", color: "Black", quantity: 6} , 
+      { size: "XXl", color: "Black", quantity: 12} , 
+
+      { size: "S", color: "Green", quantity: 6} , 
+      { size: "L", color: "Green", quantity: 6} , 
+      { size: "Xl", color: "Green", quantity: 8} , 
+
+
+    ],
+
+    images: {
+      Black: "assets/dresses/dress10-black.png",
+      Green: "assets/dresses/dress10-Green.png",
+    },
+
+    material: "Chiffon",
+    brand: "SheIn", 
+  } ,
+
+  // Accessories
+  {
+    id: 'prod_11', 
+    name: "Stainless Steel Butterfly Pendant Necklace & Earring Set", 
+    description: "3pcs Stainless Steel Butterfly Pendant Necklace & Earring Set, Fashion Gold And Silver Color Jewelry Accessories",
+    price: 200,
+    sellerId: 'seller_2', 
+    category: "Women", 
+    subCategory: "Accessories", 
+    
+    stock: [
+      { color: "Gold", quantity: 5 },
+      { color: "Silver", quantity: 5 },
+     
+    ],
+
+    images: {
+      Gold: "assets/accessories/acs1-gold.png",
+      Silver: "assets/accessories/acs1-silver.png",
+   
+    },
+
+    material: "Stainless Steel",
+    brand: "Zara", 
+  },
+  {
+    id: 'prod_12', 
+    name: "Set Stainless Steel", 
+    description: "3Pcs Set Stainless Steel Nail With Clover Bracelet & Clover Chain Anklet, Suitable For Women Daily Wear",
+    price: 100,
+    sellerId: 'seller_2', 
+    category: "Women", 
+    subCategory: "Accessories", 
+    
+    stock: [
+      { color: "Gold", quantity: 7 },
+      { color: "Silver", quantity: 5 },
+     
+    ],
+
+    images: {
+      Gold: "assets/accessories/acs2-gold.png",
+      Silver: "assets/accessories/acs2-silver.png",
+   
+    },
+
+    material: "Stainless Steel", 
+    brand: "Zara", 
+  },
+  {
+    id: 'prod_13', 
+    name: "Bangle & Ring", 
+    description: "1Pc Charm Stylish Geometric Bangle & Ring",
+    price: 50,
+    sellerId: 'seller_2', 
+    category: "Women", 
+    subCategory: "Accessories", 
+    
+    stock: [
+      { color: "Gold", quantity: 2 },
+      { color: "Silver", quantity: 5},
+     
+    ],
+
+    images: {
+      Gold: "assets/accessories/acs3-gold.png",
+      Silver: "assets/accessories/acs3-silver.png",
+   
+    },
+
+    material: "Stainless Steel", 
+    brand: "Zara", 
+  },
+  {
+    id: 'prod_14', 
+    name: "Bracelet & Cuff Ring", 
+    description: "Rhinestone Decor Cuff Bracelet & Cuff Ring",
+    price: 80,
+    sellerId: 'seller_1',
+    category: "Women", 
+    subCategory: "Accessories", 
+    
+    stock: [
+      { color: "Gold", quantity: 5},
+      { color: "Silver", quantity: 8},
+     
+    ],
+
+    images: {
+      Gold: "assets/accessories/acs4-gold.png",
+      Silver: "assets/accessories/acs4-silver.png",
+   
+    },
+
+    material: "Stainless Steel", 
+    brand: "Zara", 
+  },
+  {
+    id: 'prod_15', 
+    name: "Pendant Necklace And Clavicle Chain Set", 
+    description: "5pcs New Arrival European & American Minimalist Geometric Shaped Rhinestone Inlaid Ring, Earring, Bracelet, Pendant Necklace And Clavicle Chain Set",
+    price: 500,
+    sellerId: 'seller_1', 
+    category: "Women", 
+    subCategory: "Accessories",
+    
+    stock: [
+      { color: "Silver", quantity: 8},
+     
+    ],
+
+    images: {
+      Silver: "assets/accessories/acs5-silver.png",
+   
+    },
+
+    material: "Stainless Steel", 
+    brand: "SheIn", 
+  },
+  {
+    id: 'prod_16',  
+    name: "Jewelry Set For Women", 
+    description: "Leaf Bracelet And Ring Jewelry Set For Women",
+    price: 100,
+    sellerId: 'seller_3', 
+    category: "Women", 
+    subCategory: "Accessories", 
+    
+    stock: [
+      { color: "Gold", quantity: 5},
+      { color: "Silver", quantity: 2},
+     
+    ],
+
+    images: {
+      Gold: "assets/accessories/acs6-gold.png",
+      Silver: "assets/accessories/acs6-silver.png",
+   
+    },
+
+    material: "Stainless Steel", 
+    brand: "SheIn", 
+  },
+   {
+    id: 'prod_17', 
+    name: "Tone Coil Bangle Bracelet", 
+    description: "50pcs Matte Gold Or Silver Tone Coil Bangle Bracelet, Suitable For Women's Party And Holiday Jewelry Accessories",
+    price: 80,
+    sellerId: 'seller_3', 
+    category: "Women", 
+    subCategory: "Accessories", 
+    
+    stock: [
+      { color: "Gold", quantity: 5},
+      { color: "Silver", quantity: 7},
+     
+    ],
+
+    images: {
+      Gold: "assets/accessories/acs7-gold.png",
+      Silver: "assets/accessories/acs7-silver.png",
+   
+    },
+
+    material: "Stainless Steel", 
+    brand: "SheIn", 
+  },
+  {
+    id: 'prod_18', 
+    name: "Heart Shaped Pendant Necklace", 
+    description: "1pc Silver Heart Shaped Pendant Necklace, Jewelry Gift For Women",
+    price: 90,
+    sellerId: 'seller_2', 
+    category: "Women", 
+    subCategory: "Accessories",
+    
+    stock: [
+      { color: "Gold", quantity: 10},
+      { color: "Silver", quantity: 7},
+     
+    ],
+
+    images: {
+      Gold: "assets/accessories/acs8-gold.png",
+      Silver: "assets/accessories/acs8-silver.png",
+   
+    },
+
+    material: "Stainless Steel", 
+    brand: "SheIn",
+  },
+   {
+    id: 'prod_19', 
+    name: "Silver Leaf Cubic Zirconia Handchain", 
+    description: "1 Pc Adjustable Silver Leaf Cubic Zirconia Handchain, Luxury Bridal Connected Finger Bracelet Jewelry For Wedding",
+    price: 170,
+    sellerId: 'seller_2', 
+    category: "Women", 
+    subCategory: "Accessories", 
+    
+    stock: [
+      { color: "Gold", quantity: 10},
+      { color: "Silver", quantity: 7},
+     
+    ],
+
+    images: {
+      Gold: "assets/accessories/acs9-gold.png",
+      Silver: "assets/accessories/acs9-silver.png",
+   
+    },
+
+    material: "Zirconia", 
+    brand: "SheIn",
+  },
+  {
+    id: 'prod_20', 
+    name: "Tassel Ear Thread", 
+    description: "1pc Tassel Ear Thread",
+    price: 100,
+    sellerId: 'seller_2',
+    category: "Women", 
+    subCategory: "Accessories",  
+    
+    stock: [
+      { color: "Gold", quantity: 10},
+      { color: "Silver", quantity: 7},
+     
+    ],
+
+    images: {
+      Gold: "assets/accessories/acs10-gold.png",
+      Silver: "assets/accessories/acs10-silver.png",
+   
+    },
+
+    material: "Zirconia", 
+    brand: "SheIn", 
+  },
+]
