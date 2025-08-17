e4410bed3307335b9917fd6205d334a4de209aa3
Ahmedl-Be <ahmedabdallabeltagy@gmail.com>
2025-08-17 03:52:52 +0200

    add men products data and images
    - Created  and added suits data following the shared structure
    - Added data for  and  (pants) with the same structure
    - Added images under , , and , ensuring each set of images is mapped by product ID
    - Commented out  in  so data persists in localStorage (please review and adjust if needed)

--- data/products/products.js
+++ data/products/products.js
@@ -1,5 +1,7 @@
 /* -----------MEN DATA---------- */
-
+import { MenAcc } from "./men/men-accessories.js";
+import { MenPants } from "./men/men-bottom.js";
+import { MenSuits } from "./men/men-suits.js";
 /* ----------WOMEN DATA--------- */
     //SHOES
 import { womenShoes } from "./women/women-shoes.js";
@@ -10,6 +12,9 @@ export const products = [];
 
 
 /* -----------MEN ADDED---------- */
+products.push(...MenAcc);
+products.push(...MenPants);
+products.push(...MenSuits);
 
 /* ----------WOMEN ADDED--------- */
     //SHOES
