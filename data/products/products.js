c50f798be68333cb50a78c636b29378f66f54da9
notYAS <yasserfat7alah@gmail.com>
2025-08-17 19:30:58 +0300

    merged mariam data

--- data/products/products.js
+++ data/products/products.js
@@ -9,14 +9,15 @@ import { MenTop } from "./men/men-top.js";
 /* ----------WOMEN DATA--------- */
     //SHOES
 import { womenBags } from "./women/women-bag.js";
+    // Skirts
 import { womenSkirts } from "./women/women-bottom.js";
+    // Shoes
 import { womenShoes } from "./women/women-shoes.js";
-
     // Dresses
 import { womenDresses } from "./women/women-dresses.js";
-
     // Accessories
 import { womenAccessories } from "./women/women-accessories.js";
+    // Blouses
 import { womenBlouses } from "./women/women-top.js";
 
 
