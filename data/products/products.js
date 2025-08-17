db544fd52204e9b9813fef0f1a9e2c6899d01fff
marim <marim@innotech.com.eg>
2025-08-17 07:50:06 +0300

    add women shoes product images and update product files

--- data/products/products.js
+++ data/products/products.js
@@ -2,7 +2,10 @@
 
 /* ----------WOMEN DATA--------- */
     //SHOES
+import { womenBags } from "./women/women-bag.js";
+import { womenSkirts } from "./women/women-bottom.js";
 import { womenShoes } from "./women/women-shoes.js";
+import { womenBlouses } from "./women/women-top.js";
 
 
 /* -----------PRODUCTS---------- */
@@ -14,3 +17,6 @@ export const products = [];
 /* ----------WOMEN ADDED--------- */
     //SHOES
 products.push(...womenShoes);
+products.push(...womenBags);
+products.push(...womenSkirts);
+products.push(...womenBlouses);
